4ace5ea4f85c3b55fd5568d7229f77f8c228078a
Ryuhei Yokokawa <ryuhei@y-designs.com>
2018-09-17 17:32:36 -0700

    got it running

--- node.js
+++ node.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-var secp256k1 = require("secp256k1");
+var secp256k1 = require("./node_modules/secp256k1/build/Release/secp256k1.node");
 var EC = require('elliptic').ec;
 var ec = new EC('secp256k1');
 
